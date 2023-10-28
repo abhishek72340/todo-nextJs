05a5258d1733325ae381e1e9b2acedd5b81f793e
abhishek72340 <abhisingh.72340@gmail.com>
2023-10-28 10:46:03 +0530

    fix type error

--- app/Components/AddUsers.tsx
+++ app/Components/AddUsers.tsx
@@ -6,7 +6,7 @@ const AddUsers = () => {
  const [input,setInput]=useState<string>('')
 
  const dispatch:any=useDispatch();
- const submitHandler=(event:any)=>{
+ const submitHandler=(event)=>{
   event.preventDefault();
   input.trim()?dispatch(addUser(input)):null
   setInput('')
